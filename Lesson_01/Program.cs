71c08d91de57b77186a19799e9b539e85c8d566c
Xion <xionwin@hotmail.com>
2021-07-22 16:03:05 +0800

    Update Program.cs

--- Lesson_01/Program.cs
+++ Lesson_01/Program.cs
@@ -6,7 +6,16 @@ namespace Lesson_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var numbers = new int[] { 2, 4, 1, 6, 3, 8, 7, 0, 5, 9 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var value = numbers[i];
+                if (value % 3 != 0)
+                {
+                    Console.WriteLine(value);
+                }
+
+            }
         }
     }
 }
